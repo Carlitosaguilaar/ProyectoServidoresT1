e2769ec24be6c3a79ee15e3e99419e8a6c8e4da1
Juanjee24 <jsc0009@alu.medac.es>
2021-10-07 10:34:39 +0200

    cambios y estructura para proyecto servidores 07/10/2021

--- index.php
+++ index.php
@@ -1,29 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="estilos/CSS/app.css">
-    <link rel="stylesheet" href="estilos/CSS/header.css">
+    <title>Home</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,300;0,400;0,700;0,900;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilos/css/app.css">
+    
 
 </head>
 <body>
-    <?php require 'partials/header.php '?>
+    <?php require "partials/header.php" ?>
     <div class="contenedor_form">
-        <form action="" class="formulario">
-            <h2 class="login_titulo">Login</h2>
-            <div class="campo">
-                <input type="text" name="nombre" class="inputs" placeholder="Nombre">
-            </div>
-            <div class="campo">
-                <input type="password" name="password" class="inputs" placeholder="Contraseña">
-            </div>
-            <input type="submit" value="Login" class="boton">
-            <p class="info">¿Nuevo Usuario? <a href="form_usuarios.html">Regístrate aquí</a></p>
-        </form>
 
+        <form action="" class="formulario">
+                <h2 class="login_titulo">Login</h2>
+                <div class="campo">
+                    <input type="text" name="nombre" class="inputs" placeholder="Nombre">
+                </div>
+                <div class="campo">
+                    <input type="password" name="password" class="inputs" placeholder="Contraseña">
+                </div>
+                <div class="campo">
+                    <input type="tel" name="telefono" class="inputs" placeholder="Teléfono">
+                </div>
+                <div class="campo">
+                    <input type="email" name="email" class="inputs" placeholder="Email">
+                </div>
+                <input type="submit" value="Registro" class="boton">
+                
+            </form>
     </div>
 
 
--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,8 @@
-<header class="header">
-    <a href="">Proyecto Vehículos</a>
+<header class="headerr">
+    <a href="#">Proyecto Vehículos</a>
+    <nav class="navegador">
+        <a href="usuarios.php">Registro de Usuarios</a>
+        <a href="index.php">Login</a>
+    </nav>
 </header>
+
