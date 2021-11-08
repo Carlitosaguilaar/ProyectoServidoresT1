21e44741dd62b9f980c7a9edf4a20f8973ebfbbb
franzea <fzg0001@alu.medac.es>
2021-11-08 12:50:43 +0100

    cambio cookies2

--- cookies.php
+++ cookies.php
@@ -2,7 +2,7 @@
 
 $nombre_archivo = 'Login.php';
     if (file_exists($nombre_archivo)) {
-        echo "El último acceso fue: " . date("d F Y H:i:s. ", fileatime($nombre_archivo));
+        echo "El último acceso fue el: " . date("d F Y H:i:s. ", fileatime($nombre_archivo));
     }
 
     setcookie("iniciosesion","$nombre_archivo", time()+ (60*10));
