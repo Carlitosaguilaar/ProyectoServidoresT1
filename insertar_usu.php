0285460aeb5f920737ff62672186ce3a996a6df5
Juanjee24 <jsc0009@alu.medac.es>
2021-11-08 14:44:31 +0100

    proyecto php, cookies hechas, mejorada ruta de admin al añadir usuario.

--- insertar_usu.php
+++ insertar_usu.php
@@ -4,11 +4,12 @@
     if (!$_SESSION['username']){
        header("Location:index.php");
     }
-    
+    $user= $_SESSION['username']
 ?>
 <?php 
 
-   
+    
+
     $nombre = $_GET["nombre"];
     $email = $_GET["email"];
     $telefono = $_GET["telefono"];
@@ -18,17 +19,32 @@
     
     $inc = require "conexion_database.php";
     if($inc){
-
-        $consulta = "INSERT INTO usuarios (Nombre, Contraseña, Email, Telefono)
-        VALUES ('$nombre','$contraseña','$email', '$telefono')";
+        
+        
+        $consulta = "SELECT * FROM usuarios WHERE Nombre = '$user'";
         $results = mysqli_query($conn, $consulta);
-
+    
         if ($results){
-            header("Location:index.php");
+    
+            while ($row = $results->fetch_array()){
+    
+                $usu = $row['ID_Usuario'];
+                $contraseñaBBDD = $row['Contraseña'];
+                $admin = $row['Admin'];
+            }
         }
 
-       
+        if ($results){
+            $consulta2 = "INSERT INTO usuarios (Nombre, Contraseña, Email, Telefono)
+            VALUES ('$nombre','$contraseña','$email', '$telefono')";
+            $results2 = mysqli_query($conn, $consulta2);
+
+            if ($admin == 1){    
+                header("Location:lista_usuarios.php");
+            }
+            else{
+                header("Location:index.php");
+            }
+        }
     }
-
-
 ?>
--- partials/header.php
+++ partials/header.php
@@ -3,10 +3,18 @@
 <header class="headerr">
     <a href="#">Proyecto Vehículos</a>
     <nav class="navegador">
+        <div class="user_logout">
+
+            <i class="fas fa-user"></i>
+            <p><span><?php echo $_SESSION['username']?></span></p>
+            <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+        </div>
         
-        <i class="fas fa-user"></i>
-        <p><span><?php echo $_SESSION['username']?></span></p>
-        <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+        <div class="header_flex">
+            <p><?php echo $_COOKIE['galletas']?></p>
+
+        </div>
+        
     </nav>
 </header>
 
